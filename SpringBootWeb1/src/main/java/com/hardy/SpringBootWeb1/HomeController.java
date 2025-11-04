5e96c3ade674f398860246259bfd5c89064bf832
hardik teotia <hardik10teotia@gmail.com>
2025-11-05 00:12:54 +0530

    updated code

--- SpringBootWeb1/src/main/java/com/hardy/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/hardy/SpringBootWeb1/HomeController.java
@@ -3,7 +3,9 @@ package com.hardy.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.net.http.HttpRequest;
 
@@ -16,14 +18,17 @@ public class HomeController {
         return "index.jsp";
     }
 
-    @RequestMapping("/add")
-    public String add(HttpServletRequest request, HttpSession session){
-        System.out.println("add called");
-        int num1 = Integer.parseInt(request.getParameter("num1"));
-        int num2 = Integer.parseInt(request.getParameter("num2"));
-        int result = num1 + num2;
-        System.out.println(result);
-        session.setAttribute("result",result);
+    @RequestMapping("add")
+    public String add(int num1, int num2,
+            /*HttpSession session*/ModelAndView mav){
+//        System.out.println("add called");
+//        int num1 = Integer.parseInt(request.getParameter("num1"));
+//        int num2 = Integer.parseInt(request.getParameter("num2"));
+        int result = num1 + num2 ;
+//        System.out.println(result);
+//        session.setAttribute("result",result);
+        mav.addObject("result",result);
+        mav.setViewName("result");
         return "result.jsp";
     }
 }
